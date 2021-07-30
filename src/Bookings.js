080a4fb8b34a545382d6c23bb1abb5012f7782d3
KudahShambare <kudahshambare263@gmail.com>
2021-07-31 00:17:56 +0200

    16 Done

--- src/Bookings.js
+++ src/Bookings.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
 import FakeBookings from "./data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]); //Replaced fakebookings with []
+
+  // let apiData = [];
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me/")
+      .then(resp => {
+        return resp.json();
+      })
+      .then(data => {
+        setBookings(data);
+        //  console.log(apiData);
+      });
+    // console.log(apiData);
+  }, []);
+
+  //Old Code Below
   const search = searchVal => {
     console.info("TO DO!", searchVal);
   };
