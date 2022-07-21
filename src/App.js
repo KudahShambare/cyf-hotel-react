ac8e1d9baa1ddbb99458c78533ec86e33b387255
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-07-21 03:59:16 +0200

    Step 1 and 2 Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Heading from "./Heading";
+import Bookings from "./Bookings";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Heading />
+      <Bookings />
     </div>
   );
 };
--- src/Heading.js
+++ src/Heading.js
@@ -5,9 +5,8 @@ function Heading() {
     <div>
       <header className="App-header">CYF Hotel</header>
       <img
-        src={
-          " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYZyva3_JkEWuqZaTn-3mR2js3M7INbJH0aw&usqp=CAU"
-        }
+        id="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYZyva3_JkEWuqZaTn-3mR2js3M7INbJH0aw&usqp=CAU"
         alt="logo"
       />
     </div>
