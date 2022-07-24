0801297a62186e1fec5b749f83c88d4eb2af7461
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-07-24 22:09:21 +0200

    step 10 done

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { useState } from "react/cjs/react.production.min";
+import React, { useState } from "react";
 
 const Restaurant = () => {
   const [orders, setOrders] = useState(0);
+  const orderOne = () => {
+    return setOrders(val => val + 1);
+  };
+
   return (
     <div>
       <h3>Restaurant Orders</h3>
       <ul>
         <li>
-          Pizzas: {orders} <button className="btn btn-primary">Add</button>
+          Pizzas: {orders}{" "}
+          <button className="btn btn-primary" onClick={orderOne}>
+            Add
+          </button>
         </li>
       </ul>
     </div>
